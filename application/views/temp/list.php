e0156dc97b45041655cde6fa4e1edc2a7b09f54a
MestourBS <42637780+MestourBS@users.noreply.github.com>
2018-11-23 13:42:32 +0100

    Ajout de la liste d'apprentis
    Elle se trouve dans views/temp/

---
+++ application/views/temp/list.php
@@ -0,0 +1,23 @@
+<table>
+    <thead>
+        <tr>
+            <td><b>Nom de l'apprenti</b></td>
+            <td><b>Formation</b></td>
+            <td><b>Année</b></td>
+            <td><b>Moyenne</b></td>
+            <td><b>MSP référant</b></td>
+        </tr>
+    </thead>
+    <tbody>
+        <!-- apprentices table -->
+        <!-- put this in a loop -->
+        <tr>
+            <td>nom</td>
+            <td>formation</td>
+            <td>année</td>
+            <td>moyenne</td>
+            <td>msp</td>
+        </tr>
+        <!-- end of apprentices table -->
+    </tbody>
+</table>
