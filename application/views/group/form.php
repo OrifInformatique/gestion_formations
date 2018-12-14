c2279c3b73167656d9eeab67c22ec48f4c9a4c0b
MestourBS <42637780+MestourBS@users.noreply.github.com>
2018-12-14 14:08:44 +0100

    Use CI form functions
    Hopefully works

--- application/views/group/form.php
+++ application/views/group/form.php
@@ -1,4 +1,5 @@
-<h1><?php if(!is_null($group) && isset($group)) {
+<h1><?php $this->load->helper('form');
+if(!is_null($group) && isset($group)) {
     $update = TRUE;
     echo $this->lang->line('group_modify');
 } else {
@@ -6,53 +7,66 @@
     echo $this->lang->line('group_new');
 } ?>
 </h1>
-<form>
+<?php form_open(); ?>
     <table>
         <tr>
-            <td><label for="inputID">
-                <?php echo $this->lang->line('group_id') ?>
-            </label></td>
-            <td><input type="number" name="id" id="inputID" readonly
-                <?php if($update) echo 'value="'.$group->id.'"'; ?>></td>
+            <?php $data = new Array(
+                'name'  => 'id',
+                'id'    => 'inputID'
+            );
+            if($update) $data['value'] = $group->id;
+            echo form_label($this->lang->line('group_id'), $data['id']);
+            echo form_input($data); ?>
         </tr>
         <tr>
-            <td><label for="inputName">
-                <?php echo $this->lang->line('group_name') ?>
-            </label></td>
-            <td><input type="text" name="name_group" id="inputName"
-                <?php if($update) echo 'value="'.$group->name_group.'"'; ?>></td>
+            <?php $data = new Array(
+                'name'  => 'name_group',
+                'id'    => 'inputName'
+            );
+            if($update) $data['value'] = $group->name_group;
+            echo form_label($this->lang->line('group_name'), $data['id']);
+            echo form_input($data); ?>
         </tr>
         <tr>
-            <td><label for="inputWeight">
-                <?php echo $this->lang->line('group_weight') ?>
-            </label></td>
-            <td><input type="number" name="weight" id="inputWeight"
-                <?php if($update) echo 'value="'.$group->weight.'"'; ?>></td>
+            <?php $data = new Array(
+                'name'  => 'weight',
+                'id'    => 'inputWeight'
+            );
+            if($update) $data['value'] = $group->weight;
+            echo form_label($this->lang->line('group_weight'), $data['id']);
+            echo form_input($data); ?>
         </tr>
         <tr>
-            <td><label for="inputEliminatory">
-                <?php echo $this->lang->line('group_eliminatory') ?>
-            </label></td>
-            <td><input type="checkbox" name="eliminatory" id="inputEliminatory"
-                <?php if($update && $group->eliminatory) echo 'checked'; ?>></td>
+            <?php $data = new Array(
+                'name'  => 'eliminatory',
+                'id'    => 'inputEliminatory'
+            );
+            if($update) $data['value'] = $group->eliminatory;
+            if($update && $group->eliminatory) $data['checked'] = TRUE;
+            echo form_label($this->lang->line('group_eliminatory'), $data['id']);
+            echo form_checkbox($data); ?>
         </tr>
         <tr>
-            <td><label for="inputPosition">
-                <?php echo $this->lang->line('group_position') ?>
-            </label></td>
-            <td><input type="number" name="position" id="inputPosition"
-                <?php if($update) echo 'value="'.$group->position.'"'; ?>></td>
+            <?php $data = new Array(
+                'name'  => 'position',
+                'id'    => 'inputPosition'
+            );
+            if($update) $data['value'] = $group->position;
+            echo form_label($this->lang->line('group_position'), $data['id']);
+            echo form_input($data); ?>
         </tr>
         <tr>
-            <td><label for="inputFKParentGroup">
-                <?php echo $this->lang->line('group_parent_group') ?>
-            </label></td>
-            <td><input type="number" name="fk_parent_group" id="inputFKParentGroup"
-                <?php if($update) echo 'value="'.$group->fk_parent_group.'"'; ?>></td>
+            <?php $data = new Array(
+                'name'  => 'fk_parent_group',
+                'id'    => 'inputFKParentGroup'
+            );
+            if($update) $data['value'] = $group->fk_parent_group;
+            echo form_label($this->lang->line('group_parent_group'), $data['id']);
+            echo form_input($data); ?>
         </tr>
         <tr>
-            <td><input type="submit" name="<?php echo $this->lang->line('btn_submit') ?>"></td>
-            <td><input type="reset" name="<?php echo $this->lang->line('btn_reset') ?>"></td>
+            <td><?php echo form_submit('submit', $this->lang->line('btn_submit')); ?></td>
+            <td><?php echo form_reset('reset', $this->lang->line('btn_reset')); ?></td>
         </tr>
     </table>
 </form>
