15cd0ac95250dfb1273245821b8f72e4951c0254
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2019-01-25 08:44:40 +0100

    Revert "Add is_subject checkbox"
    This reverts commit 8a58d925b78f15db7cff13382a40a48525b324c5.

--- assets/js/module_add.js
+++
@@ -1,9 +0,0 @@
-function setDisplay(){
-	if(document.getElementById("is_subject").checked){
-		document.getElementById("div_module_number").style.display = 'none';
-	} else {
-		document.getElementById("div_module_number").style.display = '';
-	}
-}
-
-setDisplay();
