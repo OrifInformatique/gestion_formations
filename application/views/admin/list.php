32ef3bc128248764cae09ae74a60e0858792e1a1
MestourBS <42637780+MestourBS@users.noreply.github.com>
2019-02-28 15:48:02 +0100

    Add teachers menu to admin menu

--- application/views/admin/list.php
+++ application/views/admin/list.php
@@ -2,4 +2,5 @@
     <h1><?php echo $this->lang->line('admin_menu'); ?></h1>
     <a href="<?php echo base_url('admin/user_index'); ?>"><?php echo $this->lang->line('user_list'); ?></a><br>
     <a href="<?php echo base_url('admin/user_type_index'); ?>"><?php echo $this->lang->line('user_type_list'); ?></a><br>
+    <a href="<?php echo base_url('admin/teacher_index'); ?>"><?php echo $this->lang->line('nav_admin_teachers'); ?></a><br>
 </div>
